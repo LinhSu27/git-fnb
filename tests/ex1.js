dd0cdf5cef5d51edd04b7e66ea4365c955a990fc
xuanlv <xuanlv2@sapo.vn>
2025-09-17 23:40:30 +0700

    Bài ex1.js

---
+++ tests/ex1.js
@@ -0,0 +1,13 @@
+// khai báo hằng với number = 12 
+const number = 12;
+console.log(number);
+//khai báo biến name với giá trị là my number
+let name = 'my number';
+console.log(name);
+//khai báo 1 biến isEven với giá trị là sai 
+let isEven = 'false';
+console.log(isEven);
+if (number % 2 == 0){
+    isEven='true';
+}
+    console.log(isEven);
