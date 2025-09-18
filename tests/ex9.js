62d68907cbb04f9700f94676b40cd649333a552b
xuanlv <xuanlv2@sapo.vn>
2025-09-18 21:46:43 +0700

    o

--- tests/ex9.js
+++ tests/ex9.js
@@ -1,5 +1,6 @@
 // In ra các giá trị từ 25 về 12
-for (let i = 25 ; i >= 12 ; i--)
-{
-    console.log(i) ; 
+let result = "";
+for (let i = 25; i >= 12; i--) {
+  result += i + " ";
 }
+console.log(result.trim());
